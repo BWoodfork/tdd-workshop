a6b1c435b5dddaa05d5919c0c20107aa35242b0b
bwoodfork <bwoodfork@8thlight.com>
2019-07-29 16:45:08 -0500

    update fizz-buzz stuff

--- __tests__/exercise/fizz-buzz-calculator.test.js
+++ __tests__/exercise/fizz-buzz-calculator.test.js
@@ -1,7 +1,1 @@
 const calculator = require("../../src/exercise/fizz-buzz-calculator")
-
-// Write a program that returns the numbers from 1 to 100. 
-// But for multiples of three returns “Fizz” instead of the number
-// and for the multiples of five returns “Buzz”.
-// For numbers which are multiples of both three and five returns “FizzBuzz”.
-// For numbers that are not multiples of either 3 nor 5, simply returns the number.
